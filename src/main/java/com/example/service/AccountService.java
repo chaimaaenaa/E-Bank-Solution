ed4001659f53f6bc16a8b88f90881fecdec8c406
chaimaaenaa <mekayssichaimaaenaa@gmail.com>
2024-07-07 01:01:26 +0100

    add service

---
+++ src/main/java/com/example/service/AccountService.java
@@ -0,0 +1,45 @@
+package ebank.service;
+
+import ebank.entity.Account;
+import ebank.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account createAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public Optional<Account> getAccountById(Long id) {
+        return accountRepository.findById(id);
+    }
+
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    public Account updateAccount(Long id, Account accountDetails) {
+        Optional<Account> account = accountRepository.findById(id);
+        if (account.isPresent()) {
+            Account existingAccount = account.get();
+            existingAccount.setAccountType(accountDetails.getAccountType());
+            existingAccount.setBalance(accountDetails.getBalance());
+            existingAccount.setDateCreated(accountDetails.getDateCreated());
+            existingAccount.setActive(accountDetails.getActive());
+            return accountRepository.save(existingAccount);
+        }
+        return null;
+    }
+
+    public void deleteAccount(Long id) {
+        accountRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/service/TransactionService.java
@@ -0,0 +1,45 @@
+package ebank.service;
+
+import ebank.entity.Transaction;
+import ebank.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public Transaction createTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public Optional<Transaction> getTransactionById(Long id) {
+        return transactionRepository.findById(id);
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public Transaction updateTransaction(Long id, Transaction transactionDetails) {
+        Optional<Transaction> transaction = transactionRepository.findById(id);
+        if (transaction.isPresent()) {
+            Transaction existingTransaction = transaction.get();
+            existingTransaction.setTransactionDate(transactionDetails.getTransactionDate());
+            existingTransaction.setAmount(transactionDetails.getAmount());
+            existingTransaction.setTransactionType(transactionDetails.getTransactionType());
+            existingTransaction.setDescription(transactionDetails.getDescription());
+            return transactionRepository.save(existingTransaction);
+        }
+        return null;
+    }
+
+    public void deleteTransaction(Long id) {
+        transactionRepository.deleteById(id);
+    }
+}
